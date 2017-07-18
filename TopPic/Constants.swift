3be1a7ca4fbd5f7795cf625bd291c3dfdf057d70
Chad Fegley <chadfegley7@gmail.com>
2017-07-18 16:47:20 -0700

    Updated UI

---
+++ TopPic/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  TopPic
+//
+//  Created by Chad Fegley on 7/18/17.
+//  Copyright © 2017 Chad Fegley. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
